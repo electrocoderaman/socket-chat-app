45e916217cd1cbddd608f03b46664b78d8fedad4
Aman <amankrdev28@gmail.com>
2026-06-18 09:28:55 +0530

    soket io working

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 import http from "node:http";
 import { Server } from "socket.io";
+import express from "express";
+import path from "node:path";
 
 async function main() {
-  const server = http.createServer();
+  const app = express();
+  app.use(express.static(path.resolve("./public")));
 
-  const io = Server();
+  const server = http.createServer(app);
+
+  const io = new Server();
 
   io.attach(server);
+  io.on("connection", (socket) => {
+    console.log(`A new socket has connected ${socket.id}`);
 
-  server.listen(9000,() => { 
-    console.log("server is running on port 9000")
-   })
+    socket.on("user:message", (data) => {
+      console.log(`message from socket`, data);
+      socket.broadcast.emit("server:message", data);
+    });
+
+    socket.on("user:typing", (data) => {
+      socket.broadcast.emit("server:user:typing", { id: socket.id });
+    });
+  });
+
+  server.listen(9000, () => {
+    console.log("server is running on port 9000");
+  });
 }
+
+main();
